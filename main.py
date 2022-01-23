e022d731f5643b634bc48cf3ca266c19c2e7adfc
Kyle Calder <kylebcalder03@gmail.com>
2022-01-23 13:30:29 -0700

    Working on part 4

--- main.py
+++ main.py
@@ -23,6 +23,57 @@ num2 = int(input("Enter a number to divide by:"))
 split(num1, num2)
 
 
-
 # Part 3
 
+def files(a):
+    text = open('demo.txt', 'a')
+    text.write(a)
+    text.close()
+
+
+files(input("What would you like to write to the file?"))
+file = open('demo.txt', 'r')
+content = file.read()
+print(content)
+file.close()
+
+
+# Part 4
+
+def product(p):
+    products = [
+        {
+            'id': 1,
+            'Name': 'Bike',
+            'Price': 10.00,
+        },
+        {
+            'id': 2,
+            'Name': 'Car',
+            'Price': 50.00,
+        },
+        {
+            'id': 3,
+            'Name': 'Shoes',
+            'Price': 3.00,
+        },
+        {
+            'id': 4,
+            'Name': 'Belt',
+            'Price': 5.00,
+        },
+        {
+            'id': 5,
+            'Name': 'Saw',
+            'Price': 15.00,
+        }
+    ]
+    case: 'id' == 1
+    print(1 in products)
+    case: 2
+    print(2 in products)
+    case: 3
+    print('id' == 3 in products)
+
+
+product(input("Choose a product from 1-5:"))
